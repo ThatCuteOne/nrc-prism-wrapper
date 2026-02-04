a0dee3015562ded010e4035928ccc82a4a2063ac
Pizztakio <gmascuto@gmail.com>
2026-02-04 22:03:03 +0100

    fix: windows logs

--- src/tasks/get_dependencies.py
+++ src/tasks/get_dependencies.py
@@ -1,4 +1,5 @@
 from importlib.machinery import SourceFileLoader
+import os
 from pathlib import Path
 import subprocess
 import sys
@@ -12,7 +13,13 @@ filename = Path(__file__)
 loader:SourceFileLoader = getattr(sys.modules['__main__'], '__loader__', None)
 meow = loader.get_data("requirements.txt").decode('utf-8')
 
-with tempfile.NamedTemporaryFile("w") as f:
-    f.write(meow)
-    f.flush()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", f.name])
+with tempfile.NamedTemporaryFile("w",delete=False) as f:
+    try:
+        f.write(meow)
+        f.flush()
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", f.name])
+        f.close()
+    except:
+        os.remove(f.name)
+    finally:
+        os.remove(f.name)
